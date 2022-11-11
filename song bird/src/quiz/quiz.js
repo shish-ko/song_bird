0d1d7e762060acbc15198d8f8c618f255111435b
shish-ko <andrey.shishko@gmail.com>
2022-11-11 11:58:30 +0300

    fix: first attempt to make coomon audio func

--- song bird/src/quiz/quiz.js
+++ song bird/src/quiz/quiz.js
@@ -6,71 +6,86 @@ let currentLevel = -1;
 // }
 
 const answers = document.querySelectorAll('.answers__answer');
-const player = document.querySelector('.audio__controls');
-const loadingMessage = document.querySelector('.audio__loading');
-const playBtn = document.querySelector('.audio__play-pause');
-const quizAudioDurationTime = document.querySelector('.audio__total-time');
-const quizAudioCurrentTime = document.querySelector('.audio__current-time');
-const quizTrack = document.querySelector('.audio__track');
-const quizTrackCurrent = document.querySelector('.audio__play-time');
+const descriptionPlayer = document.querySelector('.description__audio');
+const mainPlayer=document.querySelector('.audio')
 const nextLevelBtn = document.querySelector('.quiz__next-level');
 const answersBlock = document.querySelector('.answers');
-const shownScore=document.querySelector('.quiz__score');
-const birdTypes=document.querySelector(".quiz__bird-types")
+const shownScore = document.querySelector('.quiz__score');
+const birdTypes = document.querySelector(".quiz__bird-types")
 let hiddenBird
-const quizAudio = new Audio();
-let isQuizAudioPlay = false;
+
 let isCorrectAnswer = false;
-let totalScore=0;
+let totalScore = 0;
 
 
-playBtn.addEventListener('click', () => { 
-  playBtn.classList.toggle('audio__play-pause_pause');
-  if (isQuizAudioPlay) {
-    isQuizAudioPlay = false;
-    quizAudio.pause();
-  } else {
-    isQuizAudioPlay = true;
-    quizAudio.play();
-    showCurrentTrackTime();
-  }
-})
 
-quizAudio.onended = () => {
-  showCurrentTrackTime();
-  isQuizAudioPlay = false;
-  playBtn.classList.remove('audio__play-pause_pause');
-};
 function durationConverter(ms) {
   const ceil = Math.ceil(ms)
   const minutes = String(Math.floor(ceil / 60)).padStart(2, '0')
   const seconds = String(ceil % 60).padStart(2, '0')
   return minutes + ':' + seconds
 }
-function showCurrentTrackTime() {
-  quizTrackCurrent.style.width = `${quizTrack.clientWidth / quizAudio.duration * quizAudio.currentTime}px`
-  if (isQuizAudioPlay) {
-    quizAudioCurrentTime.textContent = durationConverter(quizAudio.currentTime);
-    setTimeout(() => {
-      showCurrentTrackTime();
-    }, 1000);
-  }
-}
-function loadAudio(player, bird){
+const quizAudio = new Audio();
+const players=new Set();
+
+function loadAudio(player, bird) {
+  const loadingMessage = player.querySelector('.audio__loading');
+  const controls = player.querySelector('.audio__controls');
+  controls.innerHTML= '<div class="audio__play-pause"></div><div class="audio__track"><div class="audio__play-time"></div><div class="audio__time"><span class="audio__current-time">00:00</span> / <span class="audio__total-time">00:00</span></div></div>'
+  const playBtn = player.querySelector('.audio__play-pause');
+  const quizAudioDurationTime = player.querySelector('.audio__total-time');
+  const quizAudioCurrentTime = player.querySelector('.audio__current-time');
+  const quizTrack = player.querySelector('.audio__track');
+  const quizTrackCurrent = player.querySelector('.audio__play-time');
   
+  controls.hidden = true;
+  loadingMessage.hidden = false;
+  
+  players.add(new Audio(bird.audio))
+  quizAudio.src = bird.audio;
+  console.log(players)
+  let isQuizAudioPlay = false;
+  quizAudio.addEventListener('loadedmetadata', () => {
+    quizAudioDurationTime.textContent = durationConverter(quizAudio.duration);
+    quizAudioCurrentTime.textContent = "00:00";
+  });
+  quizAudio.addEventListener('canplaythrough', () => {controls.hidden = false; loadingMessage.hidden = true })
+
+  playBtn.addEventListener('click', () => {
+    playBtn.classList.toggle('audio__play-pause_pause');
+    if (isQuizAudioPlay) {
+      isQuizAudioPlay = false;
+      quizAudio.pause();
+    } else {
+      isQuizAudioPlay = true;
+      quizAudio.play();
+      showCurrentTrackTime();
+    }
+  })
+  function showCurrentTrackTime() {
+    quizTrackCurrent.style.width = `${quizTrack.clientWidth / quizAudio.duration * quizAudio.currentTime}px`
+    if (isQuizAudioPlay) {
+      quizAudioCurrentTime.textContent = durationConverter(quizAudio.currentTime);
+      setTimeout(() => {
+        showCurrentTrackTime();
+      }, 1000);
+    }
+  }
+  quizAudio.onended = () => {
+    showCurrentTrackTime();
+    isQuizAudioPlay = false;
+    playBtn.classList.remove('audio__play-pause_pause');
+  };
 }
 function setNewLevel() {
-  currentLevel += 1;
-  player.hidden = true;
-  loadingMessage.hidden = false;
+  currentLevel += 1;  
   isCorrectAnswer = false;
-  isQuizAudioPlay = false;
-  playBtn.classList.remove('audio__play-pause_pause');
-  
+  hiddenBird = birdsData[currentLevel][Math.floor(Math.random() * 6)];
+  loadAudio(mainPlayer, hiddenBird)
   birdTypes.children[currentLevel].classList.add("quiz__bird-type_current");
   nextLevelBtn.removeEventListener("click", setNewLevel);
-  document.querySelector('.audio__bird-name').textContent = "*****";
-  document.querySelector('.audio__bird-pic').src = "../assets/img/unknown-bird.jpg";
+  // document.querySelector('.audio__bird-name').textContent = "*****";
+  // document.querySelector('.audio__bird-pic').src = "../assets/img/unknown-bird.jpg";
   for (let i = 0; i < answersBlock.children.length; i++) {
     answersBlock.children[i].firstElementChild.className = "answer__color";
     delete answersBlock.children[i].quiz
@@ -78,38 +93,31 @@ function setNewLevel() {
   for (let i = 0; i < answers.length; i++) {
     answers[i].textContent = birdsData[currentLevel][i].name
   }
-  hiddenBird = birdsData[currentLevel][Math.floor(Math.random() * 6)];
-  console.time('qwe')
-  quizAudio.src = hiddenBird.audio;
-  quizAudio.addEventListener('loadedmetadata', () => {
-    quizAudioDurationTime.textContent = durationConverter(quizAudio.duration);
-    quizAudioCurrentTime.textContent ="00:00";
-  })
-
-  quizAudio.addEventListener('canplaythrough', () => { console.timeEnd('qwe'); player.hidden = false; loadingMessage.hidden = true })
 }
 setNewLevel();
 
 answersBlock.addEventListener('click', clickAnswer);
 function clickAnswer(event) {
+
   const birdId = Array.from(answersBlock.children).indexOf(event.target.closest('li')) + 1
+  loadAudio(descriptionPlayer, birdsData[currentLevel][birdId-1])
   if (birdId === hiddenBird.id) {
     document.querySelector('.audio__bird-name').textContent = hiddenBird.name;
     document.querySelector('.audio__bird-pic').src = hiddenBird.image;
-    document.querySelector('.audio__bird-pic').addEventListener("error", ()=>document.querySelector('.audio__bird-pic').src= "../assets/img/error-bird.png")
+    document.querySelector('.audio__bird-pic').addEventListener("error", () => document.querySelector('.audio__bird-pic').src = "../assets/img/error-bird.png")
     isCorrectAnswer = true;
     event.target.closest('li').firstElementChild.classList.add('answer__color_right');
     nextLevelBtn.addEventListener("click", setNewLevel);
-    let levelScore=6
-    for(let i=0; i<answersBlock.children.length; i++){
-      if(answersBlock.children[i].hasOwnProperty("quiz")) levelScore-=1;
+    let levelScore = 6
+    for (let i = 0; i < answersBlock.children.length; i++) {
+      if (answersBlock.children[i].hasOwnProperty("quiz")) levelScore -= 1;
     }
-    totalScore+=levelScore;
-    shownScore.textContent=totalScore;
+    totalScore += levelScore;
+    shownScore.textContent = totalScore;
   } else {
     if (!isCorrectAnswer) {
       event.target.closest('li').firstElementChild.classList.add('answer__color_wrong');
-      event.target.closest('li').quiz="checked";
+      event.target.closest('li').quiz = "checked";
     }
   }
 }
